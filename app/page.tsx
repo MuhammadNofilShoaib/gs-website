669df12a688a98ec75c05e93644cdec31a3366e9
Nofil Shoaib <nofilshoaib@gmail.com>
2024-11-28 00:55:29 -0800

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,40 @@
-import Image from "next/image";
+import React from 'react';
 
-export default function Home() {
+function Page() {
   return (
-    <div className="bg-cover bg-center h-auto"
-    style={{
-      backgroundImage: "url('/house.jpg')",
-    }} >
-     
-      <div className="bg-white bg-opacity-80 h-auto flex items-center justify-between px-8 ">
-        <div className="w-1/2 ml-72">
-          <h1 style={{ color: '#044E83'}}className="text-6xl font-bold font-sans mt-28 font-poppins">Governer Sindh</h1>
-          <h1 style={{ color: '#044E83'}} className="text-5xl font-thin text-blue-600 font-sans mt-1">Kamran Khan Tessori</h1>
-          <h1 style={{ color: '#2EB6E8'}} className="text-4xl font-bold text-blue-500 font-sans mt-5 ">Certified Cloud</h1>
-          <h1 style={{ color: '#2EB6E8'}} className="text-4xl font-bold text-blue-500 font-sans">Applied Generative AI</h1> 
-          <h1 style={{ color: '#2EB6E8'}} className="text-4xl font-bold text-blue-500 font-sans mb-5">Engineering (GenEng)</h1>
-          <h1 style={{ color: '#044E83'}} className="text-2xl font-bold text-blue-600 font-sans mb-3 mt-3">Earn up to $5,000 / month</h1>
-          <h1 style={{ color: '#044E83'}} className="text-2xl font-bold text-blue-600 font-sans mt-3">Now admissions are open in</h1>
-          <h1 style={{ color: '#044E83'}} className="text-2xl font-bold text-blue-600 font-sans mb-5">Hyderabad</h1> 
-      <button  style={{ backgroundColor: '#044E83'}} className="text-white px-16 py-4 rounded font-sans font-bold mb-24">APPLY NOW</button>
-        </div>
-        <div className="w-1/2">
-          <Image
-            src="/g.png" 
-            alt="Hero Image"
-            width={800} 
-            height={800} 
-            className="object-cover min-w-[900px] lg:w-[600px] mt-10"
-          />
-        </div>
+    <div className="grid justify-center items-center grid-cols-10 grid-rows-5 gap-8 p-16 text-center bg-gray-100 min-h-screen text-2xl uppercase text-white font-bold italic">
+      <div className="bg-cyan-500 h-full w-full row-span-6 rounded-lg shadow-lg hover:shadow-xl transition duration-500 transform hover:scale-105 flex items-center justify-center">
+        Box 1
       </div>
-     
+      <div className="bg-lime-500 h-full w-full col-span-8 rounded-lg shadow-lg hover:shadow-xl transition duration-500 transform hover:scale-105 flex items-center justify-center">
+        Box 2
+      </div>
+      <div className="bg-purple-500 h-full w-full row-span-6 rounded-lg shadow-lg hover:shadow-xl transition duration-500 transform hover:scale-105 flex items-center justify-center">
+        Box 3
+      </div>
+      <div className="bg-yellow-500 h-full w-full col-span-4 rounded-lg shadow-lg hover:shadow-xl transition duration-500 transform hover:scale-105 flex items-center justify-center">
+        Box 4
+      </div>
+      <div className="bg-green-500 h-full w-full col-span-4 rounded-lg shadow-lg hover:shadow-xl transition duration-500 transform hover:scale-105 flex items-center justify-center">
+        Box 5
+      </div>
+      <div className="bg-orange-500 h-full w-full row-span-4 rounded-lg shadow-lg hover:shadow-xl transition duration-500 transform hover:scale-105 flex items-center justify-center">
+        Box 6
+      </div>
+      <div className="bg-blue-500 h-full w-full col-span-7 rounded-lg shadow-lg hover:shadow-xl transition duration-500 transform hover:scale-105 flex items-center justify-center">
+        Box 7
+      </div>
+      <div className="bg-red-500 h-full w-full col-span-4 rounded-lg shadow-lg hover:shadow-xl transition duration-500 transform hover:scale-105 flex items-center justify-center">
+        Box 8
+      </div>
+      <div className="bg-emerald-500 h-full w-full col-span-3 rounded-lg shadow-lg hover:shadow-xl transition duration-500 transform hover:scale-105 flex items-center justify-center">
+        Box 9
+      </div>
+      <div className="bg-pink-500 h-full w-full col-span-7 rounded-lg shadow-lg hover:shadow-xl transition duration-500 transform hover:scale-105 flex items-center justify-center">
+        Box 10
+      </div>
     </div>
   );
 }
+
+export default Page;
